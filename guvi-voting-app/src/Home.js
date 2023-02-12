fef5d8199cc3a413222a18d7a3f5b2eae3876949
Manikandan Anbalagan <manikandan.a@analyticbrains.com>
2023-02-12 14:34:58 +0530

    task

--- guvi-voting-app/src/Home.js
+++ guvi-voting-app/src/Home.js
@@ -2,18 +2,34 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <Comment username="Manikandan" time={(new Date()).toString()}>
-      <h1>Hello Everyone!!</h1>
-      <p>This is a new comment!</p>
-    </Comment>
+    <div>
+      <Comment time={(new Date()).toString()}>
+        <h1>Hello Everyone!!</h1>
+        <p>This is a new comment!</p>
+      </Comment>
+      <Recomment name="GUVIB39-WE" time={(new Date()).toString()}>
+        <h1>This is Recomment section!!</h1>
+      </Recomment>
+    </div>
   )
 }
 
-function Comment({ username, time, children }) {
+function Comment({ username = "Hello!!! ", time, children }) {
   return (
     <section>
-      <p>{username} commented at {time}</p>
       {children}
+      <p>{username} commented at {time}</p>
+
+    </section>
+  )
+}
+
+function Recomment(props) {
+  return (
+    <section>
+      {props.children}
+      <p>{props.name} commented at {props.time}</p>
+
     </section>
   )
 }
