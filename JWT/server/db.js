8e589414d1d3548035c5573c70063a35c4db1f86
Manikandan Anbalagan <manikandan.a@analyticbrains.com>
2023-03-04 16:04:01 +0530

    JWT

---
+++ JWT/server/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  try {
+    mongoose.connect(process.env.ATLAS_URI, {});
+    console.log("Connected to database successfully!!!");
+  }
+  catch (error) {
+    console.log(error);
+    console.log('Connection Failed!!!');
+  }
+}
